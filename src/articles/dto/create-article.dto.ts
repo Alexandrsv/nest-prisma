581139f29bac1a2cc8f1b939e22e51df71700665
Александр <zbc@mail.ru>
2022-07-04 01:49:22 +0300

    add prisma queries to article methods + swagger

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,1 +1,23 @@
-export class CreateArticleDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateArticleDto {
+  @ApiProperty({
+    type: String,
+    required: true,
+    description: 'Title of the article',
+  })
+  title: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  description?: string;
+
+  @ApiProperty({
+    required: true,
+    type: String,
+    description: 'Content of the article',
+  })
+  body: string;
+
+  @ApiProperty({ required: false, default: false })
+  published?: boolean = false;
+}
